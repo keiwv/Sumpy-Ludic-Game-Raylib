06bb79d6f9a69ed5d5854f582ec26029f5833211
keiwv <141773283+keiwv@users.noreply.github.com>
2023-11-18 22:22:33 -0800

    deleted:    build/Sumpy.exe 	modified:   src/main.c

--- src/main.c
+++ src/main.c
@@ -1,48 +1,102 @@
-/*
-
-*/
-#include <math.h>
 #include "raylib.h"
+#include <math.h>
 
-int main(void)
+typedef enum
 {
-    const int pantallaAncho = 1920;
-    const int pantallaAlto = 1080;
+    MENU,
+    OPTIONS,
+    CREDITS,
+    EXIT
+} GameScene;
 
-    InitWindow(pantallaAncho, pantallaAlto, "Sumpy - Juego lúdico");
-    SetTargetFPS(144);
+GameScene currentScene = MENU;
+Vector2 mousePosition = {0};
+bool waiting = true;
 
-    bool waiting = true;
-    float fontSize = 80.0f;
+bool CheckMouseOnOption(const char *optionText, float fontSize);
 
-    Texture2D texture = LoadTexture("resources/background.png");
-    Texture2D raylibLogo = LoadTexture("resources/cubicmap.png");
-
-    Vector2 logoPosition = {1725.0f, 970.0f};
-
-    while (!WindowShouldClose())
-    {
-        ShowCursor();
-
-        BeginDrawing();
-        DrawTexture(texture, pantallaAncho / 2 - texture.width / 2, pantallaAlto / 2 - texture.height / 2, WHITE);
-        DrawFPS(10, 10);
-        DrawTextureEx(raylibLogo, logoPosition, 0, 5, WHITE);
-        if (waiting)
-        {
-            fontSize = 80.0f + 10.0f * sinf(GetTime() * 2.0f);
-            DrawText("Selecciona un nivel", pantallaAncho / 2 - MeasureText("Selecciona un nivel", fontSize) / 2, pantallaAlto / 2, fontSize, WHITE);
+void UpdateMenu() {
+    // Lógica de actualización del menú
+    if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
+        if (CheckMouseOnOption("Opciones", 70)) {
+            currentScene = OPTIONS;
+        } else if (CheckMouseOnOption("Créditos", 70)) {
+            currentScene = CREDITS;
+        } else if (CheckMouseOnOption("Salir", 70)) {
+            currentScene = EXIT;
+        } else {
+            waiting = false;
         }
-        else
-        {
-            DrawText("Selecciona un nivel", pantallaAncho / 2 - MeasureText("Selecciona un nivel", 70) / 2, pantallaAlto / 2, 70, WHITE);
+    }
+}
+
+void DrawMenu() {
+    // Lógica de dibujo del menú
+    BeginDrawing();
+    ClearBackground(BLACK);
+
+    float fontSize = 80.0f + 10.0f * sinf(GetTime() * 8.0f);
+    DrawText("Selecciona un nivel", GetScreenWidth() / 2 - MeasureText("Selecciona un nivel", fontSize) / 2, GetScreenHeight() / 2, fontSize, WHITE);
+    DrawText("Opciones", GetScreenWidth() / 2 - MeasureText("Opciones", 70) / 2, GetScreenHeight() * 0.6, 70, WHITE);
+    DrawText("Créditos", GetScreenWidth() / 2 - MeasureText("Créditos", 70) / 2, GetScreenHeight() * 0.7, 70, WHITE);
+    DrawText("Salir", GetScreenWidth() / 2 - MeasureText("Salir", 70) / 2, GetScreenHeight() * 0.8, 70, WHITE);
+
+    EndDrawing();
+}
+
+void UpdateOptions() {
+    // Lógica de actualización de opciones
+}
+
+void DrawOptions() {
+    // Lógica de dibujo de opciones
+}
+
+void UpdateCredits() {
+    // Lógica de actualización de créditos
+}
+
+void DrawCredits() {
+    // Lógica de dibujo de créditos
+}
+
+int main() {
+    InitWindow(GetScreenWidth(), GetScreenHeight(), "Sumpy");
+
+    while (!WindowShouldClose()) {
+        mousePosition = GetMousePosition();
+
+        switch (currentScene) {
+        case MENU:
+            UpdateMenu();
+            DrawMenu();
+            break;
+        case OPTIONS:
+            UpdateOptions();
+            DrawOptions();
+            break;
+        case CREDITS:
+            UpdateCredits();
+            DrawCredits();
+            break;
+        case EXIT:
+            // Lógica para salir del programa
+            break;
         }
-        DrawText("Opciones", pantallaAncho / 2 - MeasureText("Opciones", 70) / 2, pantallaAlto * 0.6, 70, WHITE);
-        DrawText("Créditos", pantallaAncho / 2 - MeasureText("Créditos", 70) / 2, pantallaAlto * 0.7, 70, WHITE);
-        DrawText("Salir", pantallaAncho / 2 - MeasureText("Salir", 70) / 2, pantallaAlto * 0.8, 70, WHITE);
-        EndDrawing();
     }
 
-    UnloadTexture(texture);
     CloseWindow();
+    return 0;
+}
+
+
+bool CheckMouseOnOption(const char *optionText, float fontSize) {
+
+    Rectangle optionBounds = MeasureTextEx(GetFontDefault(),optionText, fontSize,0);
+
+
+    optionBounds.x = GetScreenWidth() / 2 - optionBounds.width / 2;
+    optionBounds.y = GetScreenHeight() * 0.6;
+
+    return CheckCollisionPointRec(mousePosition, optionBounds);
 }
